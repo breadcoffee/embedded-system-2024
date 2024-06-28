b8764d0a20058eaad25779bc88fbde5f643bd8b8
breadcoffee <146920372+breadcoffee@users.noreply.github.com>
2024-06-28 17:28:10 +0900

    7일차 개인프젝 마무리

--- day07/iotProject04.py
+++ day07/iotProject04.py
@@ -2,6 +2,7 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.QtCore import QTimer, QTime
 import RPi.GPIO as GPIO
 import time
 
@@ -12,6 +13,8 @@ green = 4
 switch = 16
 piezoPin = 17
 servoPin = 12
+trigPin = 14
+echoPin = 15
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(red, GPIO.OUT)
@@ -20,18 +23,44 @@ GPIO.setup(green, GPIO.OUT)
 GPIO.setup(piezoPin, GPIO.OUT)
 GPIO.setup(switch, GPIO.IN)
 GPIO.setup(servoPin, GPIO.OUT)
+GPIO.setup(trigPin, GPIO.OUT)
+GPIO.setup(echoPin, GPIO.IN)
 
 servo = GPIO.PWM(servoPin, 50) # 서보핀 PWM모드 50Hz로 사용
 servo.start(0)	# 서보 PWM 시작 duty = 0, duty가 0이면 서보는 동작 x
 Buzz = GPIO.PWM(piezoPin, 440)
 
-form_class = uic.loadUiType("design03.ui")[0]
+form_class = uic.loadUiType("design04.ui")[0]
+
+def measure():
+	GPIO.output(trigPin, True)
+	time.sleep(0.00001)
+	GPIO.output(trigPin, False)
+	start = time.time()
+
+	while GPIO.input(echoPin) == False:
+		start = time.time()
+	while GPIO.input(echoPin) == True:
+		stop = time.time()
+	elapsed = stop - start
+	distance = (elapsed * 19000) / 2
+	return distance
+def door_open():
+	servo.ChangeDutyCycle(10)
+def door_close():
+    servo.ChangeDutyCycle(5)
 
 # windowClass
 class WindowClass(QMainWindow, form_class):
 	def __init__(self):			# 생성자, 첫번째 인자는 self
 		super().__init__()	# 부모클래스 생성자(QWidgets)
 		self.setupUi(self)
+		self.timer = QTimer(self)
+	def ultra_start(self):
+		self.timer.timeout.connect(self.update_distance)
+		self.timer.start(1000)
+	def ultra_stop(self):
+		self.timer.stop()
 	# 부저 스타트
 	def buzz_start(self):
 		Buzz.start(50)
@@ -42,10 +71,33 @@ class WindowClass(QMainWindow, form_class):
 	# 서보모터 제어 함수
 	def servo_open(self):
 		self.textEdit_2.setText("문이 열렸습니다!")
-		servo.ChangeDutyCycle(5)
+		door_open()
 	def servo_close(self):
 		self.textEdit_2.setText("문이 닫혔습니다!")
-		servo.ChangeDutyCycle(10)
+		door_close()
+	# 거리 감지 센서
+	def measure(self):
+		GPIO.output(trigPin, True)
+		time.sleep(0.00001)
+		GPIO.output(trigPin, False)
+		start = time.time()
+
+		while GPIO.input(echoPin) == False:
+			start = time.time()
+		while GPIO.input(echoPin) == True:
+			stop = time.time()
+		elapsed = stop - start
+		distance = (elapsed * 19000) / 2
+		return distance
+	def update_distance(self):
+		distance = measure()
+		print(distance)
+		if distance <= 5:
+			self.textEdit_3.setText("문이 열립니다")
+			door_open()
+		else:
+			self.textEdit_3.setText("문이 닫힙니다")
+			door_close()
 
 	# LED 이벤트 함수 등록
 	def red_on(self):
